2f873902f493d54a37cc951ad43d5a7f0aebdf1e
Hendrik Wichern <git@hendrikwichern.de>
2025-05-25 20:16:40 +0200

    add authentication

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -1,11 +1,22 @@
 import { ConfigSection } from "../../components/config-section/ConfigSection";
 import { Header } from "../../components/header/Header";
+import { auth0 } from "../../lib/auth0";
 import { fetchConfigs } from "../../lib/fetchConfigs";
 
 import styles from "./Manage.module.scss";
 
 export default async function Manage() {
   const configs = await fetchConfigs();
+  const session = await auth0.getSession();
+
+  if (!session) {
+    return (
+      <main>
+        <a href="/auth/login?screen_hint=signup">Sign up</a>
+        <a href="/auth/login">Log in</a>
+      </main>
+    );
+  }
 
   return (
     <div data-page className="container">
