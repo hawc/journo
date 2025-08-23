c1ca98002d17823bd4bb80332baba9b2ff6d6edf
Hendrik Wichern <git@hendrikwichern.de>
2025-08-23 17:39:20 +0200

    add literal matches to search results

--- app/api/getArticles/route.ts
+++ app/api/getArticles/route.ts
@@ -4,6 +4,8 @@ import { fetchNewsApi, NewsApiArticle } from "../../../lib/fetchNewsApi";
 import { getMatchingArticles } from "../../../lib/openAi";
 import { Article } from "../../../types/article";
 
+const ADD_LITERAL_MATCHES = true;
+
 function hashString(string: string) {
   return createHash('md5').update(string).digest('hex');
 }
@@ -20,6 +22,19 @@ function removeDuplicates(articles: Article[]) {
 
 function sortByDate(articles: Article[]) {
   return articles.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+}
+
+function appendUniqueById(articleList: Article[], newItems: Article[]) {
+  const existingIds = new Set(articleList.map(item => item._id));
+
+  for (const item of newItems) {
+    if (!existingIds.has(item._id)) {
+      articleList.push(item);
+      existingIds.add(item._id);
+    }
+  }
+
+  return articleList;
 }
 
 export async function POST(request: Request) {
@@ -50,6 +65,16 @@ export async function POST(request: Request) {
   const matchingArticleIds = await getMatchingArticles(userPrompt, articleHeadlines);
 
   const articleList = matchingArticleIds.map(id => allArticles.find(article => article._id === id)).filter(article => !!article);
+
+  if (ADD_LITERAL_MATCHES) {
+    const lowerCasePrompt = userPrompt.toLocaleLowerCase();
+
+    const filteredArticles = articles.filter(article => {
+      return article.headline.toLocaleLowerCase().includes(lowerCasePrompt) || article.content.toLocaleLowerCase().includes(lowerCasePrompt);
+    });
+
+    appendUniqueById(articleList, filteredArticles);
+  }
 
   return new Response(JSON.stringify(sortByDate(articleList)), {
     status: 201,
